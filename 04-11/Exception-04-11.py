f8a7324d966c3b786d9f68287de287b49f4abe08
Karlinskiy_v <karlinksij_v@itstep.academy>
2024-11-04 17:56:18 +0200

    04.11

---
+++ 04-11/Exception-04-11.py
@@ -0,0 +1,17 @@
+print("Prev code")
+try:
+    input()
+    print(10 / 2)
+    print(value)
+except KeyboardInterrupt:
+    print("Помилка роботи з клавіатурою")
+except ZeroDivisionError:
+    print("Не можна ділити на нуль")
+except (NameError, KeyError) as error:
+    print(error)
+
+
+print("Next code")
+
+
+
--- Student.py
+++ Student.py
@@ -2,13 +2,22 @@
 import datetime
 
 class Student:
-
-
-    def __init__(self,name, surname, birthdate, height=160):
+    def __init__(self,name:str, surname:str, birthdate, height=160):
+        #  помилка, якщо name не є str
+        if(type(name) != str):
+            raise TypeError(f"Name must be str type. Not a '{type(name)}'")
+        # помилка, якщо surname не є str
+        if (type(surname) != str):
+            raise TypeError(f"Surname must be str type. Not a '{type(surname)}'")
+        # помилка, якщо birthdate не є str
+        # помилка, якщо birthdate більший ніж поточний день
+        # помилка, якщо height не є int/float
+        # помилка, якщо height менший або дорівнює нулю
         self.name = name
         self.surname = surname
         # 25.10.2006
         self.birthdate = datetime.datetime.strptime(birthdate, '%d.%m.%Y')
+        print(self.birthdate.date() < datetime.date.today())
         self.height = height
         print(f"I am {self.name}")
 
@@ -29,7 +38,7 @@ class Student:
 
 
 # створення об'єкта
-first_student = Student('vlad', 'karlinskij', '25.10.2006',186)
+first_student = Student("vlad", 'karlinskij', '04.11.2000',186)
 second_student = Student('oleg', 'palkin', '25.5.2000',220)
 
 first_student.printStudent()
